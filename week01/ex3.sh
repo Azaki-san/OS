56c32b31b45b3fcd4eaad0fce2d7eb5d0d8425c8
Andrew <jandr21@ya.ru>
2023-09-09 22:26:29 +0300

    Exercise 3

---
+++ week01/ex3.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+date
+sleep 3
+mkdir ~/rootF
+date
+sleep 3
+mkdir ~/homeF
+date
+sleep 3
+cd ~/rootF
+touch root.txt
+date
+sleep 3
+cd ~/homeF
+touch home.txt
+date
+sleep 3
+ls -ltr / > ~/rootF/root.txt
+cat ~/rootF/root.txt
+ls
+ls -ltr ~ > ~/homeF/home.txt
+cat ~/homeF/home.txt
+ls
