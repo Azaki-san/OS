461d2f862374b3b641079088b611ff7c4ced0830
Andrew <a.alexeev@innopolis.university>
2023-10-22 22:41:02 +0300

    week 07 exercises

---
+++ week07/allocator.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MEMORY_SIZE 10000000
+
+unsigned int memory[MEMORY_SIZE];
+
+void init_memory() {
+    for (int i = 0; i < MEMORY_SIZE; i++) {
+        memory[i] = 0;
+    }
+}
+
+void clear(unsigned int adrs) {
+    for (int i = 0; i < MEMORY_SIZE; i++) {
+        if (memory[i] == adrs) {
+            memory[i] = 0;
+        }
+    }
+}
+
+void allocate_first_fit(unsigned int adrs, int size) {
+    int count = 0;
+    for (int i = 0; i < MEMORY_SIZE; i++) {
+        if (memory[i] == 0) {
+            count++;
+            if (count == size) {
+                for (int j = i - size + 1; j <= i; j++) {
+                    memory[j] = adrs;
+                }
+            }
+        } else {
+            count = 0;
+        }
+    }
+}
+
+void allocate_best_fit(unsigned int adrs, int size) {
+    int bestFitIndex = -1;
+    int bestFitSize = MEMORY_SIZE;
+    int currentFit = 0;
+    for (int i = 0; i < MEMORY_SIZE; i++) {
+        if (memory[i] == 0) {
+            currentFit++;
+        } else {
+            if (currentFit >= size && currentFit < bestFitSize) {
+                bestFitSize = currentFit;
+                bestFitIndex = i - currentFit;
+            }
+            currentFit = 0;
+        }
+    }
+
+    if (currentFit >= size && currentFit < bestFitSize) {
+        bestFitIndex = MEMORY_SIZE - currentFit;
+    }
+
+    if (bestFitIndex != -1) {
+        for (int i = bestFitIndex; i < bestFitIndex + size; i++) {
+            memory[i] = adrs;
+        }
+    }
+}
+
+void allocate_worst_fit(unsigned int adrs, int size) {
+    int worstFitIndex = -1;
+    int worstFitSize = 0;
+    int currentFit = 0;
+    for (int i = 0; i < MEMORY_SIZE; i++) {
+        if (memory[i] == 0) {
+            currentFit++;
+        } else {
+            if (currentFit >= size && currentFit > worstFitSize) {
+                worstFitSize = currentFit;
+                worstFitIndex = i - currentFit;
+            }
+            currentFit = 0;
+        }
+    }
+
+    if (currentFit >= size && currentFit > worstFitSize) {
+        worstFitIndex = MEMORY_SIZE - currentFit;
+    }
+
+    if (worstFitIndex != -1) {
+        for (int i = worstFitIndex; i < worstFitIndex + size; i++) {
+            memory[i] = adrs;
+        }
+    }
+}
+
+int main() {
+    init_memory();
+    for (int i = 0; i < 3; i++) {
+        FILE *file = fopen("queries.txt", "r");
+        if (file == NULL) {
+            perror("Error opening the file");
+            return EXIT_FAILURE;
+        }
+        char line[50];
+        clock_t start = clock();
+        int totalCount = 0;
+        while (fgets(line, sizeof(line), file)) {
+            if (strcmp(line, "end\n") == 0) {
+                break;
+            } else {
+                char query[9];
+                unsigned int adrs;
+                int size;
+                if (line[0] == 'a') {
+                    sscanf(line, "%s %u %d\n", query, &adrs, &size);
+                    if (i == 0) {
+                        allocate_first_fit(adrs, size);
+                    } else if (i == 1) {
+                        allocate_best_fit(adrs, size);
+                    } else if (i == 2) {
+                        allocate_worst_fit(adrs, size);
+                    }
+                } else if (line[0] == 'c') {
+                    sscanf(line, "%s %u\n", query, &adrs);
+                    clear(adrs);
+                }
+                totalCount++;
+            }
+        }
+        clock_t end = clock();
+        fclose(file);
+
+        double totalTime = (double) (end - start) / CLOCKS_PER_SEC;
+        double throughput = totalCount / totalTime;
+        printf("Throughput: %.2f queries/second\n", throughput);
+        init_memory();
+    }
+    return EXIT_SUCCESS;
+}
+
+
+
---
+++ week07/ex2.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#define FILE_SIZE (500 * 1024 * 1024)
+
+int main() {
+    int fd = open("text.txt", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    if (fd == -1) {
+        perror("open");
+        exit(1);
+    }
+
+    int random_fd = open("/dev/random", O_RDONLY);
+    if (random_fd == -1) {
+        perror("open");
+        close(fd);
+        exit(1);
+    }
+
+    int char_count = 0;
+    off_t file_size = 0;
+
+    while (file_size < FILE_SIZE) {
+        char c;
+        if (read(random_fd, &c, 1) != 1) {
+            perror("Failed to read text.txt");
+            close(fd);
+            close(random_fd);
+            exit(1);
+        }
+
+        if (isprint(c)) {
+            if (write(fd, &c, 1) != 1) {
+                perror("Failed to write text.txt");
+                close(fd);
+                close(random_fd);
+                exit(1);
+            }
+            char_count++;
+
+            if (char_count >= 1024) {
+                if (write(fd, "\n", 1) != 1) {
+                    perror("Failed to write text.txt");
+                    close(fd);
+                    close(random_fd);
+                    exit(1);
+                }
+                char_count = 0;
+            }
+            file_size++;
+        }
+    }
+
+    long page_size = sysconf(_SC_PAGESIZE);
+    long chunk_size = page_size * 1024;
+    off_t offset = 0;
+    int capital_count = 0;
+
+    while (offset < FILE_SIZE) {
+        char *mmapped_data = mmap(NULL, chunk_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, offset);
+
+        if (mmapped_data == MAP_FAILED) {
+            perror("Failed MMAP");
+            close(fd);
+            close(random_fd);
+            exit(1);
+        }
+
+        for (off_t i = 0; i < chunk_size; i++) {
+            if (i < FILE_SIZE - offset) {
+                if (isupper(mmapped_data[i])) {
+                    capital_count++;
+                    mmapped_data[i] = tolower(mmapped_data[i]);
+                }
+            }
+        }
+        printf("Chunk finished\n");
+
+        munmap(mmapped_data, chunk_size);
+        offset += chunk_size;
+    }
+
+    printf("Total capital letters: %d\n", capital_count);
+    close(fd);
+    close(random_fd);
+
+    return EXIT_SUCCESS;
+}
